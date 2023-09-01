f4b60e6f4757a6ded3f99b68117dbefdaf88df6b
natam_ferreira <ferreira.natamleao@gmail.com>
2023-09-01 19:12:55 -0300

    Implementação da bomba

--- src/asteroide.cpp
+++ src/asteroide.cpp
@@ -7,7 +7,7 @@ void Asteroide::asteroide(){
     glEnable(GL_BLEND); // Ativa o blending para permitir a transparência
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // Define a função de blending: fonte alfa para 1 - fonte alfa
     glEnable(GL_POINT_SMOOTH); // Ativa o suavização de pontos para renderizar pontos de forma suave
-    glPointSize(4.0); // Define o tamanho do ponto
+    glPointSize(12.0); // Define o tamanho do ponto
     glColor3f(1.0, 0.0, 0.0); // Define a cor (vermelho) do ponto
     glBegin(GL_POINTS); // Inicia o desenho de pontos
         glVertex2f(4.0, 4.0); // Desenha um ponto nas coordenadas (4.0, 4.0)
